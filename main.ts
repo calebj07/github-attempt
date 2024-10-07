216acc7eff9d2074bd2089770c1f7c89f57cb591
calebj07 <148258995+calebj07@users.noreply.github.com>
2024-10-07 12:19:39 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,23 @@
+input.onButtonPressed(Button.A, function () {
+    music.play(music.stringPlayable("B A G A G F A C5 ", 120), music.PlaybackMode.UntilDone)
+})
+input.onGesture(Gesture.Shake, function () {
+    for (let index = 0; index < 4; index++) {
+        basic.pause(100)
+        basic.showIcon(IconNames.SmallHeart)
+        basic.pause(100)
+        basic.showLeds(`
+            . # . # .
+            # . # . #
+            # . . . #
+            . # . # .
+            . . # . .
+            `)
+        basic.pause(100)
+        basic.showIcon(IconNames.Heart)
+        basic.pause(100)
+    }
+})
 basic.forever(function () {
 	
 })
